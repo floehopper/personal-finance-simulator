e6c0937f20aee2655c4095ff2464866e2f9c5bd4
James Mead <james@floehopper.org>
2011-05-26 14:38:52 +0100

    No need to expose Simulator#current_month_index any more. Also using the range provided in Simulator#play seems more sensible.

--- simulator.rb
+++ simulator.rb
@@ -1,5 +1,4 @@
 class Simulator
-  attr_reader :current_month_index
 
   def initialize
     @events = Hash.new { |hash, key| hash[key] = [] }
@@ -10,7 +9,7 @@ class Simulator
   end
 
   def schedule_in(number_of_months, &block)
-    schedule_at(current_month_index + number_of_months, &block)
+    schedule_at(@current_month_index + number_of_months, &block)
   end
 
   def schedule_each(month_offsets, &block)
@@ -20,8 +19,8 @@ class Simulator
   end
 
   def play(month_range)
-    @current_month_index = month_range.first
-    while @current_month_index <= month_range.last do
+    month_range.each do |month_index|
+      @current_month_index = month_index
       @events[@current_month_index].each do |event|
         event.call(@current_month_index)
       end
