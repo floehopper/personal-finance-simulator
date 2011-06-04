833240bf3ce776caa3765355dc2b748302e83390
James Mead <james@floehopper.org>
2011-06-04 16:13:18 +0200

    Simplify things a bit by moving PaymentCalculator classes into Loan class and giving more responsibility to the Loan factory methods.

--- app/loan.rb
+++ app/loan.rb
@@ -1,19 +1,67 @@
 class Loan
 
+  class InterestOnly
+
+    def initialize(principal_account, rate, term)
+      @principal_account, @rate, @term = principal_account, rate, term
+    end
+
+    def monthly_interest_payment
+      @principal_account.balance * @rate.per_month.to_f
+    end
+
+    def monthly_principal_repayment
+      Money.empty
+    end
+
+    def total_monthly_payment
+      monthly_interest_payment + monthly_principal_repayment
+    end
+
+  end
+
+  class Repayment < InterestOnly
+
+    def initialize(principal_account, rate, term)
+      super
+      @rate.add_observer(self)
+      update
+    end
+
+    def monthly_principal_repayment
+      total_monthly_payment - monthly_interest_payment
+    end
+
+    def total_monthly_payment
+      @total_monthly_payment
+    end
+
+    def update
+      @total_monthly_payment = monthly_interest_payment / (1 - (1 + @rate.per_month.to_f) ** -@term.in_months)
+    end
+
+  end
+
   class << self
     def interest_only(simulator, lender_account, borrower_account, amount, rate, term)
-      new(simulator, lender_account, borrower_account, amount, rate, term, PaymentCalculator::InterestOnly)
+      build(simulator, lender_account, borrower_account, amount, rate, term, InterestOnly)
     end
 
     def repayment(simulator, lender_account, borrower_account, amount, rate, term)
-      new(simulator, lender_account, borrower_account, amount, rate, term, PaymentCalculator::Repayment)
+      build(simulator, lender_account, borrower_account, amount, rate, term, Repayment)
+    end
+
+    private
+
+    def build(simulator, lender_account, borrower_account, amount, rate, term, payment_basis_class)
+      principal_account = Account.new(amount)
+      payment_basis = payment_basis_class.new(principal_account, rate, term)
+      new(simulator, lender_account, borrower_account, principal_account, rate, term, payment_basis)
     end
   end
 
-  def initialize(simulator, lender_account, borrower_account, amount, rate, term, payment_calculator_class)
-    @simulator, @lender_account, @borrower_account, @rate, @term = simulator, lender_account, borrower_account, rate, term
-    @principal_account = Account.new(amount)
-    @payment_calculator = payment_calculator_class.new(@principal_account, @rate, @term)
+  def initialize(simulator, lender_account, borrower_account, principal_account, rate, term, payment_basis)
+    @simulator, @lender_account, @borrower_account, @principal_account, @rate, @term, @payment_basis = simulator, lender_account, borrower_account, principal_account, rate, term, payment_basis
   end
 
   def draw_down
@@ -23,8 +71,8 @@ class Loan
   end
 
   def process_monthly_payment
-    Transfer.new(@borrower_account, @lender_account, @payment_calculator.total_monthly_payment).complete
-    @principal_account.debit(@payment_calculator.monthly_principal_repayment)
+    Transfer.new(@borrower_account, @lender_account, @payment_basis.total_monthly_payment).complete
+    @principal_account.debit(@payment_basis.monthly_principal_repayment)
     @term.reduce_months_by(1)
   end
 
